12867badc49dfa9aae8075be23a44f9664192f7b
Michael Talarczyk <michael.talarczyk@onepeloton.com>
2021-05-25 11:39:57 -0700

    Add logging, attempt LoggingConnection

--- tasks.py
+++ tasks.py
@@ -1,24 +1,31 @@
 from invoke import Collection, task
+from psycopg2.extras import DictCursor, LoggingConnection
 
+import logging
 import psycopg2
-import psycopg2.extras
 import queries
 import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def setup_database(c, dbname=None):
     if dbname:
         c.repack.dbname = dbname
-    c.config.conn = psycopg2.connect(dbname=c.repack.dbname, host='localhost')
+    c.config.conn = psycopg2.connect(
+        connection_factory=LoggingConnection,
+        dbname=c.repack.dbname,
+        host='localhost')
+    c.config.conn.initialize(logger)
     c.config.conn.set_session(autocommit=True)
     with c.config.conn.cursor() as cursor:
         cursor.execute('CREATE EXTENSION IF NOT EXISTS pg_repack;')
 
 
 def find_bloated_table(c):
-    dict_cur = psycopg2.extras.DictCursor
-    with c.config.conn.cursor(cursor_factory=dict_cur) as cursor:
-        cursor.execute(queries.show_database_bloat())
+    with c.config.conn.cursor(cursor_factory=DictCursor) as cursor:
+        cursor.execute(queries.show_database_bloat(), [1000000])
         return cursor.fetchone()
     return None
 
@@ -41,13 +48,13 @@ def repack(c, dbname=None, table=None):
     Repack the specified table, or auto-select one if not specified.
     """
     setup_database(c, dbname)
-    print(f'INFO: dbname={c.repack.dbname}')
+    logger.info(f'dbname={c.repack.dbname}')
 
     if table:
         c.repack.table = table
     else:
         result = find_bloated_table(c)
-        print('INFO: table={} tbloat={} wastedbytes={}'.format(
+        logger.info('table={} tbloat={} wastedbytes={}'.format(
             result['tablename'], result['tbloat'], result['wastedbytes']))
         print('Ctrl-C to abort or repack in 5 seconds...')
         time.sleep(5)
